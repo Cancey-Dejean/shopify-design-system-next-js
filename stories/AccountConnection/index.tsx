2e26d335e39f487cc70569b623baa40a2405b830
Cancey Dejean <canceyd@gmail.com>
2024-08-30 22:20:38 -0500

    Added more structure to AccountConnection component

--- stories/AccountConnection/index.tsx
+++ stories/AccountConnection/index.tsx
@@ -7,6 +7,7 @@ export default function AccountConnection({
 }: {
   title?: string;
   accountName?: string;
+  handleAction?: () => void;
 }) {
   const [connected, setConnected] = useState(false);
 
@@ -15,14 +16,32 @@ export default function AccountConnection({
   }, []);
 
   return (
-    <div>
-      <div>
-        <div className="flex justify-between">
-          <div className="flex gap-2">
-            <p>{connected ? accountName : ""}</p>
+    <div className="rounded-[var(--p-border-radius-300)] bg-[var(--p-color-bg-surface)] p-4 shadow-[var(--p-shadow-100)] [font-size:var(--p-font-size-325)]">
+      <div className="flex flex-col justify-end gap-4">
+        <div className="flex justify-between gap-4">
+          <div className="flex gap-4">
+            {connected && (
+              <span className="flex size-7 gap-2 rounded-[0.375rem] bg-[var(--p-color-avatar-five-bg-fill)]">
+                <svg viewBox="0 0 40 40" className="size-full">
+                  <text
+                    x="50%"
+                    y="50%"
+                    dy="0.35em"
+                    fill="currentColor"
+                    text-anchor="middle"
+                    className="[color:var(--p-color-bg-surface)]"
+                  >
+                    {accountName?.slice(0, 2).toUpperCase()}
+                  </text>
+                </svg>
+              </span>
+            )}
+
             <div>
-              <h1>{title || "Example App"}</h1>
-              <p>{connected ? "Account connected" : "No account connected"}</p>
+              <p>{title || "Example App"}</p>
+              <p className="[color:var(--p-color-text-secondary)]">
+                {connected ? "Account connected" : "No account connected"}
+              </p>
             </div>
           </div>
 
